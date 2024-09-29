0e0f1d99ce842a5f4a5a3a113f5bb4eeb9ee8721
Yurilord <Fritz-h.mann@protonmail.com>
2024-09-30 00:30:55 +0200

    streamlining code, tyding up & renaming variables for clarity - based on proposed solution

--- main.py
+++ main.py
@@ -1,49 +1,55 @@
 def main():
-    text = get_content()
-    words = get_word_count()
-    characters = get_character_count()
-    compiled = compile_report(characters)
-    print_report(words, compiled)  
+    book_path = "books/frankenstein.txt"
+    text = get_book_content(book_path)
+    num_words = get_word_count(text)
+    characters_dict = get_character_count(text)
+    compiled = compile_report(characters_dict)
+    print_report(book_path, num_words, compiled)  
 
-def get_content():
-    with open("books/frankenstein.txt") as file:
+
+def get_book_content(book_path):
+    with open(book_path) as file:
         return file.read()    
 
-def get_word_count():
-    words = get_content().split()
-    return len(words)
 
-def get_character_count():
+def get_word_count(text):
+    return len(text.split())
+
+
+def get_character_count(text):
     character_count = {}
-    text = get_content().lower().split()
 
-    for word in text:
-        for character in word:
-            if character in character_count:
-                character_count[character] += 1
-            elif character not in character_count:
-                character_count[character] = 1
+    for character in text.lower():
+        if character in character_count:
+            character_count[character] += 1
+        else:
+            character_count[character] = 1
 
     return character_count
+
 
 def sort_on(dict):
     return dict["num"]
 
-def compile_report(characters):
+
+def compile_report(characters_dict):
     sorted = []
-    for char in characters:
+    for char in characters_dict:
         if char.isalpha():
-            sorted.append({"c": char, "num" : characters[char]})
+            sorted.append({"c": char, "num" : characters_dict[char]})
     sorted.sort(reverse=True, key=sort_on)
     return sorted
 
-def print_report(words,compiled):
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{words} words found in the document")
+
+def print_report(book_path, num_words, compiled):
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{num_words} words found in the document")
     print("")
     for char in compiled:
         print(f"The '{char['c']}' character was found {char['num']} times")
 
     print("--- End report ---")
+
+
 main()
 
